edf115ef034ecb943adee9e8dad3ca4ce05fd730
DJ-no1 <darkjargon739@gmail.com>
2024-10-06 17:00:56 +0530

    first commit

---
+++ src/app/(app)/upload/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const uploadVideos = () => {
+  return (
+    <div>admin upload kar sakta hai </div>
+  )
+}
+
+export default uploadVideos
---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const landingPage = () => {
+  return (
+    <div>
+      <button>hello</button>
+      <Button
+        variant="secondary"
+      >hello</Button>
+    </div>
+  )
+}
+
+export default landingPage
